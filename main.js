f353d7a3a2e0c5aae3af42dafb16f7d85fc2f82d
Kam-Dino <100112393+Kam-Dino@users.noreply.github.com>
2024-12-21 16:46:33 +0500

    change second sale container

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
-function correctDate (date){
+function correctDate(date) {
     return date < 10 ? "0" + date : date;
 }
 
+function saleDate(containerId, finishDate) {
+    const container = document.getElementById(containerId);
+    const days = container.getElementsByClassName("time")[0];
+    const hours = container.getElementsByClassName("time")[1];
+    const minutes = container.getElementsByClassName("time")[2];
+    const seconds = container.getElementsByClassName("time")[3];
 
-function saleDate(){
-    const day = document.getElementsByClassName("time")[0];
-    const hour = document.getElementsByClassName("time")[1];
-    const minute = document.getElementsByClassName("time")[2];
-    const second = document.getElementsByClassName('time')[3];
-    const finishDate = new Date(2024, 11, 31)
-    const currentDate = new Date()
-    const date = finishDate-currentDate
-    day.innerHTML = correctDate(Math.trunc(date/(1000*60*60*24)))
-    hour.innerHTML = correctDate(Math.trunc(
-        (date % (1000*60*60*24)) / (1000 * 60 * 60)));
-    minute.innerHTML = correctDate(Math.trunc((date%(1000*60*60))/(1000*60)));
-    second.innerHTML = correctDate(Math.trunc((date%(1000*60))/(1000)));
-}   
-saleDate();
-setInterval(function(){
-    saleDate();
-}, 1000)
+    const currentDate = new Date();
+    const timeDifference = finishDate - currentDate;
+
+    if (timeDifference <= 0) {
+        days.innerHTML = "00";
+        hours.innerHTML = "00";
+        minutes.innerHTML = "00";
+        seconds.innerHTML = "00";
+        return;
+    }
+
+    days.innerHTML = correctDate(Math.trunc(timeDifference / (1000 * 60 * 60 * 24)));
+    hours.innerHTML = correctDate(Math.trunc((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    minutes.innerHTML = correctDate(Math.trunc((timeDifference % (1000 * 60 * 60)) / (1000 * 60)));
+    seconds.innerHTML = correctDate(Math.trunc((timeDifference % (1000 * 60)) / 1000));
+}
+
+// Устанавливаем дату завершения
+const finishDate = new Date(2024, 11, 31); // 31 декабря 2024
+
+// Обновляем оба таймера
+setInterval(() => {
+    saleDate("timer1", finishDate);
+    saleDate("timer2", finishDate);
+}, 1000);
